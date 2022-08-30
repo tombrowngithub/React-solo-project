68d4f48985631775fbfdffad35c8c008dc2cc931
TOM-BROWN <tombrownanuma@gmail.com>
2022-08-30 14:17:14 +0100

    first commit

--- src/component/Header.js
+++
@@ -1,9 +0,0 @@
-import Earth from '../images/earth.svg'
-export default function (){
-    return(
-        <div className="nav">
-            <img src={Earth} alt=""/>
-            <h1>My Travel Journal</h1>
-        </div>
-    )
-}
